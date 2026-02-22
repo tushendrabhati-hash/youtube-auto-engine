130be19ace9676be8522abd2bc587b8aaa294678
tushendrabhati-hash <tushendrabhati@gmail.com>
2026-02-22 06:09:25 +0530

    Update channel_picker.py

--- channel_picker.py
+++ channel_picker.py
@@ -2,7 +2,7 @@ import json
 import random
 
 def get_random_channel():
-    with open("channels.json") as f:
-        data = json.load(f)
+    with open("upload_channels.json", "r") as f:
+        channels = json.load(f)
 
-    return random.choice(data["channels"])
+    return random.choice(channels)
